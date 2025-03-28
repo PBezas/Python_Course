fd156e9cd05d26f9d4046569091b6b7cf5add14d
Panos Bezas <panos.b@kissmybutton.gr>
2025-03-28 16:40:05 +0200

    fix imports in geuess_logic file and import quotes from csv file

--- web_scraping/guess_author_game/guess_logic.py
+++ web_scraping/guess_author_game/guess_logic.py
@@ -1,7 +1,7 @@
 from get_random_quote import get_random_quote
-from get_all_quotes import get_all_quotes
+from read_quotes_from_csv import read_quotes_from_csv
 
-all_quotes = get_all_quotes()
+all_quotes = read_quotes_from_csv()
 random_quote = get_random_quote(all_quotes)
 
 print(random_quote["quote"])
@@ -16,30 +16,30 @@ while i < guesses:
     if answer != random_quote["author"]:
         if i == 1:
             print(
-                f"Remaining guesses: {guesses - (i)}\n Hint{i}: Born on {random_quote['birthdate']}"
+                f"Remaining guesses: {guesses - (i + 1)}\n Hint{i}: Born on {random_quote['birthdate']}"
             )
             i += 1
         elif i == 2:
             print(
-                f"Remaining guesses: {guesses - (i)}\n Hint{i}: Born {random_quote['birthplace']}"
+                f"Remaining guesses: {guesses - (i + 1)}\n Hint{i}: Born {random_quote['birthplace']}"
             )
             i += 1
         elif i == 3:
             name_first = random_quote["author"].split(" ")[0][0]
             print(
-                f"Remaining guesses: {guesses - (i)}\n Hint{i}: First name starts with: {name_first}"
+                f"Remaining guesses: {guesses - (i + 1)}\n Hint{i}: First name starts with: {name_first}"
             )
             i += 1
         elif i == 4:
             lastname_first = random_quote["author"].split(" ")[1][0]
             print(
-                f"Remaining guesses: {guesses - (i + 1)}\n Hint{i + 1}: Last name starts with {lastname_first}"
+                f"Remaining guesses: {guesses - (i + 1)}\n Hint{i}: Last name starts with {lastname_first}"
             )
             i += 1
         else:
             print(f"You lose! The author was {random_quote['author']}")
             start_again = input("Do you want to play again?(y/n): ")
-            if start_again == "y":
+            if start_again.lower() in ("y", "yes"):
                 i = 1
                 random_quote = get_random_quote(all_quotes)
                 print(random_quote["quote"])
@@ -47,9 +47,9 @@ while i < guesses:
                 print("Thank you for playing. See you next time!")
                 break
     else:
-        print("Congrats!")
+        print("Congrats! You got it right!")
         start_again = input("Do you want to play again?(y/n): ")
-        if start_again == "y":
+        if start_again.lower() in ("y", "yes"):
             i = 1
             random_quote = get_random_quote(all_quotes)
             print(random_quote["quote"])
