76b64bf1cb98fc33f1a4da34290cb1cdd5b271b5
Panos Bezas <p.bezas1@gmail.com>
2025-04-13 11:57:47 +0300

    add web_scraping project

--- web_scraping/guess_author_game/get_author_details.py
+++ web_scraping/guess_author_game/get_author_details.py
@@ -5,7 +5,6 @@ from scrape_quotes import BASE_URL
 
 def get_author_details(quote):
     bio_page_url = BASE_URL + quote["bio_link"]
-
     bio_page_html = requests.get(bio_page_url)
     bio_soup = BeautifulSoup(bio_page_html.text, "html.parser")
     birthday = bio_soup.find(class_="author-born-date").get_text()
